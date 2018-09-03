0923165f1df59d5004d564575dc3b8f22f9f3966
squarerock <dev.squarerock@gmail.com>
2018-09-02 22:53:51 -0700

    Variables: Kotlin

--- src/Variables.kt
+++ src/Variables.kt
@@ -1,7 +1,42 @@
 /**
  * Created by pranavkonduru on 9/2/18.
  */
-class Variables {
 
+/**
+ * Constructor is defined in the class definition
+ * ? after type indicates that the variable can be null
+ */
+class Variables (var randomMap : Map<String, String>?) {
+
+    /**
+     * Custom getter.
+     * If randomMap was null, it will not execute .size
+     */
+    val isMapEmpty get() = randomMap?.size == 1
+
+    /**
+     * Custom getter.
+     * Backing field is needed to set fields
+     */
+    var counter = 0
+        set(value) {
+           if(value > 0) field = value
+        }
+
+    /**
+     * Static methods or fields are defined
+     */
+    companion object {
+        /**
+         * Not a compile time constant.
+         * Will generate getters and setters
+         */
+        val RANDOM_CONSTANT = 15132
+
+        /**
+         * Compile time constant
+         */
+        const val RANDOM_STRING = "hello!"
+    }
 
 }
