462b4d7d67ede896aa313e035181b9583e1c1e21
squarerock <dev.squarerock@gmail.com>
2018-09-05 16:25:14 -0700

    making data class similar to java class

--- src/classes/Movie.kt
+++ src/classes/Movie.kt
@@ -4,8 +4,8 @@ package classes
  * Created by pranavkonduru on 9/2/18.
  */
 
-data class Movie(val name: String,
-                 val director: String,
-                 var rating : Float,
-                 val genre : String = "comedy",
-                 val yearReleased : String = "2018")
+data class Movie @JvmOverloads constructor(val name: String,
+                                           val director: String,
+                                           var rating : Float?,
+                                           val genre : String = "comedy",
+                                           val yearReleased : String = "2018")
